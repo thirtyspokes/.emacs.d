9bbc3fce7cea711c3fc808f5dc9f9bab013d64a0
Ray Ashman Jr <ray@rayashmanjr.com>
2019-02-01 19:28:40 -0500

    more junk

--- init.el
+++ init.el
@@ -10,16 +10,15 @@
 (setq backup-directory-alist `(("." . "~/.emacs.d/saves")))
 (setq backup-by-copying t)
 
-;; font
+;; font and colors
 (set-default-font "Vulf Mono 20")
+(load-theme 'gruvbox-light-hard t)
 
 ;; hide splash screen
 (setq inhibit-startup-message t inhibit-startup-echo-area-message t)
 
 ;; line numbers
-(add-hook 'prog-mode-hook
-	  (lambda ()
-	    (display-line-numbers-mode 1)))
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
 ;; command is meta
 (setq mac-command-modifier 'meta)
@@ -28,18 +27,12 @@
 (ido-mode 1)
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
-
-(load-theme 'gruvbox-light-hard t)
+(ido-vertical-mode 1)
+(setq ido-vertical-define-keys 'C-n-and-C-p-only)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (go-mode gruvbox-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages (quote (ido-vertical-mode go-mode gruvbox-theme))))
