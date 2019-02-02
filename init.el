70a3f2abaedd1b5f55f958655fc2dd758f40bbbb
Ray Ashman Jr <ray@rayashmanjr.com>
2019-02-01 19:21:19 -0500

    add an ignore and some other stuff

--- init.el
+++ init.el
@@ -6,6 +6,10 @@
 (tool-bar-mode -1)
 (scroll-bar-mode)
 
+;; put backups in just one place goddamn
+(setq backup-directory-alist `(("." . "~/.emacs.d/saves")))
+(setq backup-by-copying t)
+
 ;; font
 (set-default-font "Vulf Mono 20")
 
@@ -32,7 +36,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (gruvbox-theme))))
+ '(package-selected-packages (quote (go-mode gruvbox-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
